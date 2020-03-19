b5a48793b92effe64a72d6ad272a1dbe9aa2c4c6
Sopheap Sopheadavid <sopheap.sopheadavid@optistech.com>
2020-03-19 10:04:17 +0700

    move action sheet widget to folder widget

--- lib/screens/widget/task_list.dart
+++ lib/screens/widget/task_list.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:todoey/models/task.dart';
 import 'package:todoey/provider/task.dart';
 import 'package:todoey/screens/widget/task_tile.dart';
+import 'package:todoey/utils/widget/action_sheet.dart';
 
 class TaskList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final TaskProvider _taskProvider = Provider.of<TaskProvider>(context);
-    if(_taskProvider.taskCount > 0){
       return Consumer<TaskProvider>(
         builder: (context, _taskProvider, child){
           return Scrollbar(
@@ -26,39 +25,12 @@ class TaskList extends StatelessWidget {
                     deleteBoxCallback: () => showCupertinoModalPopup(
                         context: context,
                         builder: (context) =>
-                            _buildActionSheet(context, _taskProvider, index, _taskProvider.taskList[index])
+                            ShowActionSheet(_taskProvider, index, _taskProvider.taskList[index])
                     ),
                   );
                 }),
           );
         },
       );
-    }else{
-      return CircularProgressIndicator();
-    }
-  }
-
-  Widget _buildActionSheet(
-      BuildContext context, TaskProvider _taskProvider, int index, Task task) {
-    return CupertinoActionSheet(
-      title: Text("Do you really want to delete it?"),
-      actions: <Widget>[
-        CupertinoActionSheetAction(
-          child: Text("Delete"),
-          isDestructiveAction: true,
-          onPressed: () {
-            _taskProvider.deleteTask(index, task);
-            Navigator.pop(context);
-          },
-        ),
-        CupertinoActionSheetAction(
-          child: Text("Cancel"),
-          isDefaultAction: true,
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ],
-    );
   }
 }
--- lib/utils/widget/action_sheet.dart
+++ lib/utils/widget/action_sheet.dart
@@ -1,8 +1,35 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:todoey/models/task.dart';
+import 'package:todoey/provider/task.dart';
 
 class ShowActionSheet extends StatelessWidget {
+  Task task;
+  int index;
+  TaskProvider taskProvider;
+  ShowActionSheet(this.taskProvider, this.index, this.task);
+
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text("Hello World"),);
+    return CupertinoActionSheet(
+      title: Text("Do you really want to delete it?"),
+      actions: <Widget>[
+        CupertinoActionSheetAction(
+          child: Text("Delete"),
+          isDestructiveAction: true,
+          onPressed: () {
+            taskProvider.deleteTask(index, task);
+            Navigator.pop(context);
+          },
+        ),
+        CupertinoActionSheetAction(
+          child: Text("Cancel"),
+          isDefaultAction: true,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ],
+    );
   }
 }
